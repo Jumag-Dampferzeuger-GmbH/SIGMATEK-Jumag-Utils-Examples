f843c5da13853016559375b3d84b88dad1d57582
Alexander Tonn <alexandertonn@gmx.de>
2024-09-25 16:29:10 +0200

    delay.c modified for getting the true elapsed time. Until now the elapsed time only were a timestamp of the ops.tabsolute

--- delay/Source/delay.c
+++ delay/Source/delay.c
@@ -1,10 +1,11 @@
-/* 
+/*
 
 Function Global __cdecl DelayTon
 VAR_INPUT
   IN : BOOL;
-  PT : DINT; 
-  ET : ^UDINT; 
+  PT : DINT;
+  ET : ^UDINT;
+  TIMESTAMP : ^UDINT;
   CLOCK : UDINT;
 END_VAR
 VAR_OUTPUT
@@ -12,31 +13,39 @@ VAR_OUTPUT
 END_VAR;
 
 */
-// turn on the output Q after the input IN has been on for the time PT. 
+// turn on the output Q after the input IN has been on for the time PT.
 // The time is measured in CLOCK units. The elapsed time is stored in ET.
-extern "C" bool DelayTon (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
+extern "C" bool DelayTon(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
-    if (!xCondition) 
+  if (!condition)
+  {
+    *elapsed = 0; 
+    *timestamp = clock;
+    return false;
+  }
+  else
+  {
+    if (clock > (*timestamp + targetTime))
     {
-        *uiElapsedTime = uiClock; 
-        return false;
+      *elapsed = targetTime;
+      return true;
     }
     else
     {
-        if (uiClock > (*uiElapsedTime + iTargetTime)) 
-            return true;
-        else
-            return false;
+      *elapsed = clock - *timestamp;
+      return false;
     }
+  }
 }
 
-/* 
+/*
 
 Function Global __cdecl DelayTof
 VAR_INPUT
   IN : BOOL;
-  PT : DINT; 
-  ET : ^UDINT; 
+  PT : DINT;
+  ET : ^UDINT;
+  TIMESTAMP : ^UDINT;
   CLOCK : UDINT;
 END_VAR
 VAR_OUTPUT
@@ -45,18 +54,26 @@ END_VAR;
 
 */
 // turn on the output for a certain time if IN Condition is true
-extern "C" bool DelayTof (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
+extern "C" bool DelayTof(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
-    if (xCondition) 
+  if (condition)
+  {
+    *elapsed = 0; 
+    *timestamp = clock;
+    return true;
+  }
+  else
+  {
+    if (clock > (*timestamp + targetTime))
     {
-        *uiElapsedTime = uiClock; 
-        return true; 
+      *elapsed = targetTime;
+      return false;
     }
+
     else
-    {    
-        if (uiClock > (*uiElapsedTime + iTargetTime)) 
-            return false;
-        else
-            return true; 
+    {
+      *elapsed = clock - *timestamp;
+      return true;
     }
+  }
 }
