7fcd89f3f7a32fc86287730d7de2571b8e565321
Alexander Tonn <alexandertonn@gmx.de>
2025-01-17 16:45:53 +0100

    Added new example for timeConversion.c source file

---
+++ timeConversion/Source/timeConversion.c
@@ -0,0 +1,34 @@
+/*DECLARATION IN LASAL CLASS 2
+
+
+Function Global __cdecl ms_to_s
+VAR_INPUT
+  millis : UDINT;
+END_VAR;
+VAR_OUTPUT
+  seconds : UDINT;
+END_VAR;
+
+*/
+extern "C" long unsigned int ms_to_s (const long unsigned int millis)
+{
+    return millis / 1000; 
+}
+
+/*DECLARATION IN LASAL CLASS 2
+
+
+Function Global __cdecl ms_to_min
+VAR_INPUT
+  millis : UDINT;
+END_VAR;
+VAR_OUTPUT
+  minutes : UDINT;
+END_VAR;
+
+
+*/
+extern "C" long unsigned int ms_to_min (const long unsigned int millis)
+{
+    return millis / 60000; 
+}
